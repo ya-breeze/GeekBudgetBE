ef18c2d9c99ee4f2702a05307de8509166d0cadb
Ilya Korolev <ilya.korolev@outlook.com>
2024-09-09 20:40:14 +0000

    fix: Make login field unique in User model

--- pkg/database/models/user.go
+++ pkg/database/models/user.go
@@ -13,7 +13,7 @@ type User struct {
 
 	ID             uuid.UUID `gorm:"type:uuid;primaryKey"`
 	StartDate      time.Time
-	Login          string
+	Login          string `gorm:"unique"`
 	HashedPassword string
 }
 
