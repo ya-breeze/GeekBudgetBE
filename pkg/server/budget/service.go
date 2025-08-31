109149afb01ce28004cfde684e992677d66658b9
Eliáš Král <ilya.korolev@outlook.com>
2025-08-31 10:02:37 +0000

    feat: Implement ValidateFutureMonth - Add validation to ensure budget dates are in future months only

--- pkg/server/budget/service.go
+++ pkg/server/budget/service.go
@@ -1,7 +1,9 @@
 package budget
 
 import (
+	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/ya-breeze/geekbudgetbe/pkg/database"
 )
@@ -16,4 +18,18 @@ func NewService(logger *slog.Logger, db database.Storage) *Service {
 		logger: logger,
 		db:     db,
 	}
+}
+
+// ValidateFutureMonth ensures the given monthStart is in the future (not past or current month)
+func (s *Service) ValidateFutureMonth(monthStart time.Time) error {
+	now := time.Now()
+	// Get first day of next month
+	nextMonth := time.Date(now.Year(), now.Month()+1, 1, 0, 0, 0, 0, now.Location())
+
+	if monthStart.Before(nextMonth) {
+		return fmt.Errorf("budget month must be in the future, got %s but minimum is %s",
+			monthStart.Format("2006-01"), nextMonth.Format("2006-01"))
+	}
+
+	return nil
 }
