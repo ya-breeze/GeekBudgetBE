d21e49a4a5d5b2fbf9acaf1e81c7dc546d5fbca3
Eliáš Král <ekral@purestorage.com>
2025-12-15 23:26:51 +0100

    fix: FE warnings

--- frontend/src/app/features/budget-items/components/budget-matrix-edit/budget-matrix-edit.component.ts
+++ frontend/src/app/features/budget-items/components/budget-matrix-edit/budget-matrix-edit.component.ts
@@ -5,7 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CurrencyPipe, DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 
 export interface BudgetMatrixEditData {
@@ -25,7 +25,7 @@ export interface BudgetMatrixEditData {
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    CurrencyPipe,
+
     DatePipe
   ],
   template: `
